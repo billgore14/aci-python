0ae2aa254a590f26a7ba474230c19064b9f540c3
Dunieski Otano <yanetperez2@aol.com>
2021-09-23 12:48:59 -0400

    Create conversions.py
    Pushing new data

---
+++ Conversions/conversions.py
@@ -0,0 +1,35 @@
+import os
+from os import system
+system('cls')
+
+x = 10
+print()
+print(f'''
+    
+    Type of x is  {type(x)}
+    
+    ''')
+x = float(x)
+print(f'''
+    
+    Type of x is now  {type(x)}
+    
+    ''')
+x = int(x)
+print(f'''
+    
+    Type of x is now  {type(x)}
+    
+    ''')
+string = "True"
+print(f'''
+    
+    Type of string is now  {type(string)}
+    
+    ''')
+string = bool(string)
+print(f'''
+    
+    Type of string is now  {type(string)}
+    
+    ''')
