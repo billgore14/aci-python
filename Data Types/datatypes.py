9beb58ca54a2d37b477a20a950a6eb41531448e9
Dunieski Otano <yanetperez2@aol.com>
2021-09-23 12:49:04 -0400

    Update datatypes.py
    Pushing new data

--- Data Types/datatypes.py
+++ Data Types/datatypes.py
@@ -11,8 +11,12 @@ floatNum = 2.54 # float type with 32 bits => 4 bytes
 stringSingle = 'My dog is a beautiful one => single quotes' # string class (single quotes)
 stringDouble = "My dog is a beautiful one => double quotes" # string class (double quotes)
 stringTriple = '''
+    
+    
     My dog is a beautiful one
     (triple quotes)
+
+
 '''
 stringNum = "56 => still a string"
 isTrue = True # a boolean
@@ -29,3 +33,5 @@ print(f"String => Double quotes = {stringDouble}")
 print(f"String => Triple quotes = {stringTriple}")
 print(f"Boolean 1 = {isTrue}")
 print(f"Boolean 2 = {isStudent}")
+
+
